bb364cf54f108e11e0ef56a2ffeb7966fcda6d6d
DangNg <nguyenhuudang00@gmail.com>
2017-03-06 02:10:19 +0700

    Update extract_SQLAccount.sh

--- extract_SQLAccount.sh
+++ extract_SQLAccount.sh
@@ -3,7 +3,8 @@
 
 for i in `ls /home/*/wp-config.php`
 do
-         #echo $i  #ten thu muc
+         domain=`echo $i | awk -F "/" '{ print $3}'`
+         echo "Domain: $domain"
          user=`cat $i | grep DB_USER | awk -F "'" '{ print $4}' `
          database=`cat $i | grep DB_NAME |  awk -F "'" '{ print $4}' `
          password=`cat $i | grep DB_PASSWORD |  awk -F "'" '{ print $4}' `
