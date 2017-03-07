14a70317609c75b2706af9d82889aa779ff19350
DangNg <nguyenhuudang00@gmail.com>
2017-03-07 17:41:34 +0700

    Create restore_Databases.sh

---
+++ restore_Databases.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for i in `ls /home/*/public_html/wp-config.php`
+do
+         domain=`echo $i | awk -F "/" '{ print $3}'`
+         echo "Domain: $domain"
+         user=`cat $i | grep DB_USER | awk -F "'" '{ print $4}' `
+         database=`cat $i | grep DB_NAME |  awk -F "'" '{ print $4}' `
+         password=`cat $i | grep DB_PASSWORD |  awk -F "'" '{ print $4}' `
+         echo $user $password $database
+		     sql_file="$database.sql"
+		     `/usr/bin/mysql -u $user -p$password $database < /root/database/$sql_file`
+done
