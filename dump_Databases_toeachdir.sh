d4c1e9579cccdd454e94ddc05ce799a8b760a6f2
DangNg <nguyenhuudang00@gmail.com>
2017-03-08 11:53:43 +0700

    Update dump_Databases_toeachdir.sh

--- dump_Databases_toeachdir.sh
+++ dump_Databases_toeachdir.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in `ls /home/*/wp-config.php`
+for i in `ls /home/*/public_html/wp-config.php`
 do
          domain=`echo $i | awk -F "/" '{ print $3}'`
          echo "Domain: $domain"
@@ -8,6 +8,6 @@ do
          database=`cat $i | grep DB_NAME |  awk -F "'" '{ print $4}' `
          password=`cat $i | grep DB_PASSWORD |  awk -F "'" '{ print $4}' `
          echo $user $password $database
-	       sql_file="$database.sql"
-	       `/usr/bin/mysqldump --opt -u $user -p$password $database > /home/$domain/$sql_file`
+	 sql_file="$database.sql"
+	 `/usr/bin/mysqldump --opt -u $user -p$password $database > /home/$domain/$sql_file`
 done
